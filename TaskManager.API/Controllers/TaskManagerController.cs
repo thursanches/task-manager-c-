e771072510d30f2c38b194c7d89a7e36ac2bc2b2
Arthur Sanches <Arthursks7@gmail.com>
2024-04-25 22:58:20 -0300

    API project

---
+++ TaskManager.API/Controllers/TaskManagerController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManager.Application.UseCases.Tasks.Delete;
+using TaskManager.Application.UseCases.Tasks.GetAll;
+using TaskManager.Application.UseCases.Tasks.GetById;
+using TaskManager.Application.UseCases.Tasks.Register;
+using TaskManager.Application.UseCases.Tasks.Update;
+using TaskManager.Communication.Communication.Responses;
+using TaskManager.Communication.Requests;
+using TaskManager.Communication.Responses;
+
+namespace TaskManager.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+
+public class TaskManagerController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType((typeof(ResponseRegisterTaskJson)), StatusCodes.Status201Created)]
+    [ProducesResponseType((typeof(ResponseErrorsJson)), StatusCodes.Status400BadRequest)]
+    public IActionResult Register([FromBody] RequestRegisterTaskJson request)
+    {
+        var useCase = new RegisterTaskUseCase();
+
+        var response = useCase.Execute(request);
+
+        return Created(string.Empty, response);
+    }
+
+    [HttpPut]
+    [Route("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(ResponseErrorsJson), StatusCodes.Status400BadRequest)]
+    public IActionResult Update([FromRoute]int id, [FromBody] RequestRegisterTaskJson request)
+    {
+        var useCase = new UpdateTaskUseCase();
+
+        useCase.Execute(id, request);
+
+        return NoContent();
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(ResponseAllTaskJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+
+    public IActionResult GetAll()
+    {
+        var useCase = new GetAllTaskUseCase();
+
+        var response = useCase.Execute();
+
+        if (response.Tasks.Any())
+        {
+            return Ok(response);
+        }
+
+        return NoContent();
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    [ProducesResponseType(typeof(ResponseTaskJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResponseErrorsJson), StatusCodes.Status404NotFound)]
+    public IActionResult GetById(int id)
+    {
+        var useCase = new GetTaskByIdUseCase();
+
+        var response = useCase.Execute(id);
+
+        return Ok(response);
+    }
+
+    [HttpDelete]
+    [Route("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(ResponseErrorsJson), StatusCodes.Status200OK)]
+
+    public IActionResult Delete(int id)
+    {
+        var useCase = new DeleteTaskByUdUseCase();
+
+        useCase.Execute(id);
+
+        return NoContent();
+    }
+}
